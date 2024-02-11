dd58f0c9879e4212d71b0bfa323de42c6aacbbfa
JamesVpog <james.vongpha@gmail.com>
2024-02-10 21:15:55 -0700

    added stub for diagnosi patient by id

--- backend/api.py
+++ backend/api.py
@@ -6,6 +6,20 @@ db = TinyDB('backend/db.json') # connect to the db file
 def get_patients():
     return db.all()
 
+# use create an endpoint that diagnose problem by patient's medical record
+# uses 2 endpoints:
+# GET /patients/{ID} -> grab patient medical record by ID
+# POST /diagnose -> diagnose the paitent's medical record with GPT 
+
+def get_diagnosis_by_id(ID):
+    patient = get_patient_by_ID(ID) # JSON of patient's medical record
+    # call GPT to diagnose patient's medical record
+    # return the diagnosis/more questions
+    #diagnosis = 
+    
+    print(patient)
+    return patient, 200
+
 def get_patient_by_ID(ID):
     print(ID)
     return db.search(Query().ID == ID)
